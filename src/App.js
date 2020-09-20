e0f9644df586eb54d98974d753e2903b48cee453
Priyesh2109 <49438857+Priyesh2109@users.noreply.github.com>
2020-09-20 14:30:01 +0100

    learning about Link, useState and now building out the searchpage!

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Recommend from "./Recommend";
+import SearchPage from "./SearchPage";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
@@ -11,8 +12,11 @@ function App() {
       <Router>
         <Header />
         <Switch>
-          <Route path="/search">
-            <h1> HOME PAGE</h1>
+          <Route path="/search/:searchTerm">
+            <div className="app_page">
+              <Sidebar />
+              <SearchPage />
+            </div>
           </Route>
           <Route path="/">
             <div className="app_page">
--- src/Header.js
+++ src/Header.js
@@ -1,26 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
-import MenuSharpIcon from "@material-ui/icons/MenuSharp";
+
+import MenuIcon from "@material-ui/icons/Menu";
 import SearchIcon from "@material-ui/icons/Search";
 import VideoCallIcon from "@material-ui/icons/VideoCall";
 import AppsIcon from "@material-ui/icons/Apps";
 import NotificationsIcon from "@material-ui/icons/Notifications";
+import { Link } from "react-router-dom";
 
 function Header() {
+  const [inputSearch, setInputSearch] = useState("");
+
   return (
     <div className="header">
       <div className="header_left">
-        <MenuSharpIcon />
-        <img
-          className="header_logo"
-          src="https://cdn.mos.cms.futurecdn.net/SytNGv3ZxAVCkvcspmbbvh-320-80.jpg"
-          alt=""
-        />
+        <MenuIcon />
+        <Link to="/">
+          <img
+            className="header_logo"
+            src="https://cdn.mos.cms.futurecdn.net/SytNGv3ZxAVCkvcspmbbvh-320-80.jpg"
+            alt=""
+          />
+        </Link>
       </div>
 
       <div className="header_input">
-        <input placeholder="search" type="text" />
-        <SearchIcon className="header_searchbtn" />
+        <input
+          onChange={(e) => setInputSearch(e.target.value)}
+          value={inputSearch}
+          placeholder="search"
+          type="text"
+        />
+        <Link to={`/search/${inputSearch}`}>
+          <SearchIcon className="header_searchbtn" />
+        </Link>
       </div>
 
       <div className="header_icons">
