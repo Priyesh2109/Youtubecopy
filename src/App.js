806decc1edc0412ece0a301f626d51aedcd3e468
Priyesh2109 <49438857+Priyesh2109@users.noreply.github.com>
2020-09-30 16:01:15 +0100

    adding a new component-undecided on feature

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+
 import Sidebar from "./Sidebar";
 import Recommend from "./Recommend";
 import SearchPage from "./SearchPage";
